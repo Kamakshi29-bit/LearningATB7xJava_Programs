c93ce059d76fc9a9d3736120fc38333c1d1862ce
Kamakshi <rajavatkamakshi@gmail.com>
2024-07-13 09:29:05 +0530

    All code files

---
+++ src/exe_07062024/Lab001.java
@@ -0,0 +1,7 @@
+package exe_07062024;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/exe_07062024/Lab003.java
@@ -0,0 +1,13 @@
+package exe_07062024;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        //comment single
+        /**
+         * multiple comment
+         *    for documentation
+          */
+
+
+    }
+}
