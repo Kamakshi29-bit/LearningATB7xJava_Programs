2500680470519d7c1f76d4ee8102e23d25388d3e
Kamakshi <rajavatkamakshi@gmail.com>
2024-07-13 09:59:37 +0530

    New files are added

---
+++ src/exe_07062024/Lab007.java
@@ -0,0 +1,8 @@
+package exe_07062024;
+
+public class Lab007 {
+    public static void main(String[] args) {
+        System.out.println("Hello World, Kamakshi");
+        System.out.println("Hello World, Kamakshi");
+    }
+}
---
+++ src/exe_07062024/Lab008.java
@@ -0,0 +1,8 @@
+package exe_07062024;
+
+public class Lab008 {
+    public static void main(String[] args) {
+        //args can be any name i.e abc
+        //if multiple main functions - JVM will recognize only 1st and ignore others
+    }
+}
