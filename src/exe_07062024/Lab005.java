b83aab5e35a854be6274c9006d79cf26fb35c0f2
Kamakshi <rajavatkamakshi@gmail.com>
2024-07-13 09:53:01 +0530

    New files are added

--- src/exe_07062024/Lab005.java
+++ src/exe_07062024/Lab005.java
@@ -1,3 +1,5 @@
+package exe_07062024;
+
 public class Lab005 {
     public static void main(String[] args) {
 
---
+++ src/exe_07062024/Lab006.java
@@ -0,0 +1,10 @@
+package exe_07062024;
+
+public class Lab006 {
+    public static void main(String[] args) {
+        //Keywords and Identifiers
+        // class name should be same as code name
+        //class name - alphanumeric followed by digits
+
+    }
+}
